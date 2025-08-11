c704f8550142ad1e3c5004ae1af4c499efcc0afc
Kacper Gajko <kacper.gajko1@icloud.com>
2025-08-12 00:14:16 +0200

    fixed telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,5 +4,19 @@ return {
 		cmd = "Telescope",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
+
+config = function()
+			require("telescope").setup({
+				on_clear = function() end,
+pickers = {
+      find_files = {
+        find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
+        layout_config = {
+          height = 0.70
+        }
+      },
+		}
+			})
+		end,
 	},
 }
